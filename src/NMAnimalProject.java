c60ce7af6f69a8a9b82aa44cd4d5c021683d1205
Normonge <nmarroquin.smdc@gmail.com>
2022-10-03 22:25:27 -0400

    Commit

--- src/NMAnimalProject.java
+++ src/NMAnimalProject.java
@@ -1,13 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 public class NMAnimalProject {
     public static void main(String[] args){
 
-       Bird bird = new Bird();
-       Reptile Reptile = new Reptile();
-       Fish Fish = new Fish();
 
-       bird.speak();
-       Reptile.speak();
-       Fish.speak();
+    }
+
+    public List<Animal> readCSV(String fname){
+
+        List<Animal> Animals = new ArrayList<>();
+        File myFile = new File(fname);
+
+        try{
+            Scanner scanner = new Scanner(myFile);
+
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+
+                String[] animal = line.split(",");
+
+                Animal ani = createAnimal(animal);
+                Animals.add(ani);
+
+
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+            return Animals;
+    }
+
+    private Animal createAnimal(String[] metadata) {
+        String animal_name = metadata[];
+
     }
 
 }
